3a27bd77dd28fa98c06dece0b16814367ac445ff
dustin.luk <dustin.luk@flexsystem.com>
2018-04-12 14:29:17 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { MnFullpageModule } from 'ngx-fullpage';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
 
 import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MnFullpageModule.forRoot(),
+    MDBBootstrapModule.forRoot(),
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
